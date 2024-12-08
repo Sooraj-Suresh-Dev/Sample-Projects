e6ab8cf4ac49d152d73bbb13ab5a156998cc134d
Sooraj Suresh <soorajsuresh9597@gmail.com>
2024-12-08 13:58:52 +0530

    scrollBtn styled and added java script for scroll

--- yarloon.js
+++ yarloon.js
@@ -17,3 +17,19 @@ $(document).ready(function()
 
 
 })
+
+function up(){
+    window.location='#top';
+}
+
+
+ var myScrollFunc = function () {
+     var y = window.scrollY;
+     if (y >= 500) {
+         document.getElementById("ic").style.display='block';
+     } else {
+         document.getElementById("ic").style.display='none';
+    }
+ };
+
+ window.addEventListener("scroll", myScrollFunc);
